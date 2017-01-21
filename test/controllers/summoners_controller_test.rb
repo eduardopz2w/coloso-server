e6dfcb0a60bef34f8dc5c315981c4e9624ade97f
Alberto Pedron <pedron.albert@gmail.com>
2017-01-21 10:49:24 -0400

    Add some test cases

--- test/controllers/summoners_controller_test.rb
+++ test/controllers/summoners_controller_test.rb
@@ -33,4 +33,71 @@ class SummonersControllerTest < ActionDispatch::IntegrationTest
   test '#findById should response not_found' do
     testNotFoundResponse('/riot-api/lan/summoner/0')
   end
+
+  test '#runes should response OK' do
+    get '/riot-api/lan/summoner/75119/runes'
+    assert_response(:success)
+    res = JSON.parse(@response.body)['runes']
+    assert_instance_of(Hash, res)
+    assert_instance_of(Array, res['pages'])
+    res['pages'].each { |page|
+      assert_instance_of(String, page['name'])
+      assert_instance_of(Array, page['runes'])
+      page['runes'].each { |rune|
+        assert_instance_of(Integer, rune['runeId'])
+        assert_instance_of(Integer, rune['count'])
+        assert(rune['count'].between?(1, 9))
+        assert_instance_of(String, rune['name'])
+        assert_instance_of(String, rune['description'])
+        assert_instance_of(String, rune['image']['full'])
+      }
+    }
+  end
+
+  test '#runes should response NOT_FOUND' do
+    testNotFoundResponse('/riot-api/lan/summoner/0/runes')
+  end
+
+  test '#masteries should response OK' do
+    get '/riot-api/lan/summoner/75119/masteries'
+    assert_response(:success)
+    res = JSON.parse(@response.body)['masteries']
+    assert_instance_of(Hash, res)
+    assert_instance_of(Array, res['pages'])
+    res['pages'].each { |page|
+      assert_instance_of(String, page['name'])
+      assert_instance_of(Array, page['masteries'])
+      page['masteries'].each { |mastery|
+        assert_instance_of(Integer, mastery['id'])
+        assert(mastery['rank'].between?(1, 5))
+      }
+    }
+  end
+
+  test '#masteries should response NOT_FOUND' do
+    testNotFoundResponse('/riot-api/lan/summoner/0/masteries')
+  end
+
+  test '#statsSummary should response OK' do
+    get '/riot-api/lan/summoner/75119/stats/summary', params: { :season => 'SEASON2016' }
+    assert_response(:success)
+    res = JSON.parse(@response.body)['statsSummary']
+    assert_instance_of(Hash, res)
+    assert_instance_of(Array, res['playerStatSummaries'])
+    assert_equal('SEASON2016', res['season'])
+    res['playerStatSummaries'].each { |summary|
+      assert_instance_of(String, summary['playerStatSummaryType'])
+      assert_instance_of(Hash, summary['aggregatedStats'])
+    }
+  end
+
+  test '#statsSummary of notFound summoner should response empty stats' do
+    get '/riot-api/lan/summoner/0/stats/summary', params: { :season => 'SEASON2016' }
+    assert_response(:success)
+    res = JSON.parse(@response.body)['statsSummary']
+    assert_instance_of(Hash, res)
+    assert_instance_of(Array, res['playerStatSummaries'])
+    assert_equal(0, res['playerStatSummaries'].length)
+    assert_equal('SEASON2016', res['season'])
+  end
 end
