089680be774277a835b60b5cbb0dabea6db4f62a
Alberto Pedron <pedron.albert@gmail.com>
2017-01-26 19:39:46 -0400

    Remove unused field

--- db/migrate/20170117191446_create_pro_summoners.rb
+++ db/migrate/20170117191446_create_pro_summoners.rb
@@ -2,7 +2,6 @@ class CreateProSummoners < ActiveRecord::Migration[5.0]
   def change
     create_table :pro_summoners do |t|
       t.string :summonerUrid, unique: true, null: false
-      t.string :region
       t.integer :lastCheck, :limit => 8
       t.belongs_to :pro_player
       t.timestamps
