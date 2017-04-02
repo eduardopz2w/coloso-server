b93302be81075cfaa907bc2999cb0848949627e5
Alberto Pedron <pedron.albert@gmail.com>
2017-04-02 01:13:58 -0400

    Fix raisl admin

--- config/application.rb
+++ config/application.rb
@@ -8,14 +8,11 @@ Bundler.require(*Rails.groups)
 
 module Coloso
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
     ActiveModelSerializers.config.adapter = :json_api
     ActiveModelSerializers.config.key_transform = :camel_lower
     config.autoload_paths << "#{Rails.root}/app/errors"
     config.autoload_paths << "#{Rails.root}/app/riot"
-    config.api_only = true
+    config.middleware.use Rack::MethodOverride
     config.i18n.default_locale = 'en'
     config.androidMinApkVersion = ENV['COLOSO_MIN_APK_VERSION']
     config.androidActualApkVersion = ENV['COLOSO_ACTUAL_APK_VERSION']
