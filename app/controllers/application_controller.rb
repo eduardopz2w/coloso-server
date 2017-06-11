5b50d62825964b7f860d37cb17d50235276e1519
Alberto Pedron <pedron.albert@gmail.com>
2017-06-11 14:47:35 -0400

    Add portuguese riot data

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,10 @@ class ApplicationController < ActionController::Base
   before_action :setLocale
 
   def setLocale
+    locales = ['en', 'es', 'pt']
     requestLocale = request.headers['Accept-Language']
 
-    if requestLocale == 'es' || requestLocale == 'en'
+    if locales.include?(requestLocale)
       I18n.locale = requestLocale
     end
   end
